306b047c11e35a853ccbfe145809161861e4a95c
Balram Lodhi <balram.lodhi@walmart.com>
2026-01-13 17:57:57 +0530

    Customize the theme to get darker grays

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,3 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
-import useData from './useData';
 import genres from '../data/genres';
 
 
